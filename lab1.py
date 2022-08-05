b40b9f6f3eb9e66bb43500977bdcdb009c93e934
mayuras7685 <mayurasodara@gmail.com>
2022-08-05 23:15:01 +0530

    Lab 1

---
+++ lab1.py
@@ -0,0 +1,10 @@
+from traceback import print_tb
+
+x = int(input())
+
+if x>0 :
+  print("Given Number is Positive")
+elif x<0 :
+  print("Given Number is Negative")
+else:
+  print("Given Number is Zero")
