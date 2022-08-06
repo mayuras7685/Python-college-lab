9fb77a6947c3066e02f3d1f8d6f3d69444ac5563
mayuras7685 <mayurasodara@gmail.com>
2022-08-06 21:31:34 +0530

    update lab 2

--- lab2.py
+++ lab2.py
@@ -1,6 +1,82 @@
-str1 = "my name is mayur"
+"""4.	REPEAT ABOVE PROGRAM 
+TO USE CONTINUE AND PASS STATEMENT FOR LETTER ‘O’.
 
-if "mayur" in str1 :
-  print("True")
-else:
-  print("False")
+"""
+
+#for continue 
+str2 = "ABOCDEOFGHIGKLMNOPQRSTUVWXYZ"
+
+for i in str2:
+  if i =="O":
+    continue
+  print(i)
+
+
+"""
+output :
+A
+B
+C
+D
+E
+F
+G
+H
+I
+G
+K
+L
+M
+N
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z
+"""
+
+#for pass
+
+for i in str2:
+  if i =="O":
+    pass
+  print(i)
+
+"""
+output :
+A
+B
+O
+C
+D
+E
+O
+F
+G
+H
+I
+G
+K
+L
+M
+N
+O
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z
+"""
+
