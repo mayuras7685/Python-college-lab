d42744f730aec41da96ab50df38455a611e74b71
mayuras7685 <mayurasodara@gmail.com>
2022-08-06 21:36:54 +0530

    update lab 2

--- lab2.py
+++ lab2.py
@@ -80,3 +80,17 @@ Y
 Z
 """
 
+"""5.	WRITE A PROGRAM TO ADD THE NUMBER 
+STORED IN ARRAY TILL THE COUNTER REACHES TO USER DEFINED COUNT
+"""
+
+a = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16]
+b = int(input())
+
+sum = 0
+if b in a:
+  for i in range(1, b+1):
+    sum = sum + i
+  print(sum)
+else:
+  print(b,"is not in array")
