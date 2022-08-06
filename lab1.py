da15bde8592c0c5b751ab53a35dbbd0b0d006a94
mayuras7685 <mayurasodara@gmail.com>
2022-08-06 21:24:19 +0530

    update lab 1

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
-from traceback import print_tb
+""" 1.	INPUT NUMBER THROUGH THE USER AND FIND OUT WHETHER THE NUMBER IS ZERO,
+POSITIVE OR NEGATIVE."""
 
-x = int(input())
+x = float(input())
 
 if x>0 :
   print("Given Number is Positive")
@@ -8,3 +9,43 @@ elif x<0 :
   print("Given Number is Negative")
 else:
   print("Given Number is Zero")
+
+# output for 5 is Given Number is Positive
+
+"""2.	INITIALIZE THE ARRAY WITH THE STRING. INPUT STRING THROUGH THE USER AND 
+OUTPUT TRUE OR FALSE BASED ON THE PRESENCE OR ABSENCE OF STRING."""
+
+str1 = "My name is mayur"
+if "mayur" in str1:
+  print("True")
+else:
+  print("False")
+
+# output : True
+
+"""3.	INITIALIZE THE VARIABLE WITH STRING. PRINT ALL THE CHARACTERS UNTIL A
+ LETTER ‘O’ IS ENCOUNTERED USING BREAK STATEMENT. """
+ 
+str2 = "ABCDEFGHIGKLMNO"
+
+for i in str2:
+  if i =="O":
+    break
+  print(i)
+
+""" output : A
+B
+C
+D
+E
+F
+G
+H
+I
+G
+K
+L
+M
+N
+"""
+
